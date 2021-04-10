d60ca8ea85a99d5231543da51421a588ba13a14c
kenethfv <81479808+kenethfv@users.noreply.github.com>
2021-04-10 10:47:50 -0600

    Version 1
    Inicio de Aplicaciones

---
+++ Proyecto/src/test/java/com/Programacion/Proyecto/ProyectoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Programacion.Proyecto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProyectoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
