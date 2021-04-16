c90589728fe8857ee93f19b634e914dfd1cd9c0e
Dgo2021 <chinchillaesau@gmail.com>
2021-04-15 22:09:59 -0600

    Tablas
    Tabla Estado y Vuelo Cliente

--- Proyecto/src/main/java/com/application/proyecto/entity/Estado.java
+++ Proyecto/src/main/java/com/application/proyecto/entity/Estado.java
@@ -1,5 +1,52 @@
 package com.application.proyecto.entity;
 
-public class Estado {
+import java.io.Serializable;
 
+import javax.persistence.Basic;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "Estado")
+public class Estado implements Serializable{
+
+	private static final long serialVersionUID = 2L;
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Basic(optional = false)
+	@Column(name = "IdEstado")
+	private Integer IdEstado;
+	
+	@Column(name = "Descripcion")
+	private String Descripcion;
+
+	/*
+	 * Mapeo GET AND SET
+	 */
+	
+	public Integer getIdEstado() {
+		return IdEstado;
+	}
+
+	public void setIdEstado(Integer idEstado) {
+		IdEstado = idEstado;
+	}
+
+	public String getDescripcion() {
+		return Descripcion;
+	}
+
+	public void setDescripcion(String descripcion) {
+		Descripcion = descripcion;
+	}
+
+	
+	
+	
+	
 }
--- Proyecto/src/main/java/com/application/proyecto/entity/VueloCliente.java
+++ Proyecto/src/main/java/com/application/proyecto/entity/VueloCliente.java
@@ -1,5 +1,31 @@
 package com.application.proyecto.entity;
 
-public class VueloCliente {
+import java.io.Serializable;
 
+import javax.persistence.Basic;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "Vuelo_Cliente")
+public class VueloCliente implements Serializable{
+	
+	private static final long serialVersionUID = 3L;
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Basic(optional = false)
+	@Column(name = "idVuelo_Cliente")
+	private Integer idVuelo_Cliente;
+	
+	@Column(name = "NoBoletos")
+	private Integer NoBoletos;
+	
+	@Column(name = "Total")
+	private Integer Total;
+	
 }
