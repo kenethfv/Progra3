159337a8dce7c90db79f7533caeef1ddc36697ab
Dgo2021 <chinchillaesau@gmail.com>
2021-04-15 21:32:16 -0600

    Tabla Pais
    Tabla pais

--- Proyecto/src/main/java/com/application/proyecto/entity/Pais.java
+++ Proyecto/src/main/java/com/application/proyecto/entity/Pais.java
@@ -1,5 +1,31 @@
 package com.application.proyecto.entity;
 
-public class Pais {
+import java.io.Serializable;
 
+import javax.persistence.Basic;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "Pais")
+public class Pais implements Serializable{
+
+	private static final long serialVersionUID = 1L;
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Basic(optional = false)
+	@Column(name = "idPais")
+	private Integer idPais;
+	
+	@Column(name = "NomPais")
+	private String NomPais;
+	
+	
+	
+	
 }
