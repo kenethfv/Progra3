6dc830ef3224844020188a0650f6c8f68874710e
mescobary <82556124+mescobary@users.noreply.github.com>
2021-04-16 21:41:47 -0600

    Entity Cliente

--- Proyecto/src/main/java/com/application/proyecto/entity/Cliente.java
+++ Proyecto/src/main/java/com/application/proyecto/entity/Cliente.java
@@ -1,5 +1,78 @@
 package com.application.proyecto.entity;
 
-public class Cliente {
+import java.io.Serializable;
+
+import javax.persistence.Basic;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "cliente")
+
+public class Cliente implements Serializable {
+
+	private static final long serialVersionUID = 4L;
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Basic(optional = false)
+	@Column(name = "idcliente")
+	private Integer idcliente;
+
+	@Column(name = "nombrecliente")
+	private String nombrecliente;
+
+	@Column(name = "correocliente")
+	private String correocliente;
+
+	@Column(name = "passwordcliente")
+	private String passwordcliente;
+
+	@Column(name = "sexo")
+	private String sexo;
+
+	public Integer getIdcliente() {
+		return idcliente;
+	}
+
+	public void setIdcliente(Integer idcliente) {
+		this.idcliente = idcliente;
+	}
+
+	public String getNombrecliente() {
+		return nombrecliente;
+	}
+
+	public void setNombrecliente(String nombrecliente) {
+		this.nombrecliente = nombrecliente;
+	}
+
+	public String getCorreocliente() {
+		return correocliente;
+	}
+
+	public void setCorreocliente(String correocliente) {
+		this.correocliente = correocliente;
+	}
+
+	public String getPasswordcliente() {
+		return passwordcliente;
+	}
+
+	public void setPasswordcliente(String passwordcliente) {
+		this.passwordcliente = passwordcliente;
+	}
+
+	public String getSexo() {
+		return sexo;
+	}
+
+	public void setSexo(String sexo) {
+		this.sexo = sexo;
+	}
 
 }
