441970e295c3ebc776c9cc0751848e2d5e75f441
mescobary <82556124+mescobary@users.noreply.github.com>
2021-04-16 21:49:22 -0600

    Entity Proveedor

--- Proyecto/src/main/java/com/application/proyecto/entity/Proveedor.java
+++ Proyecto/src/main/java/com/application/proyecto/entity/Proveedor.java
@@ -1,5 +1,77 @@
 package com.application.proyecto.entity;
 
-public class Proveedor {
+import java.io.Serializable;
+
+import javax.persistence.Basic;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "proveedor")
+public class Proveedor implements Serializable {
+
+	private static final long serialVersionUID = 5L;
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Basic(optional = false)
+	@Column(name = "idproveedor")
+	private Integer idproveedor;
+
+	@Column(name = "nombreproveedor")
+	private String nombreproveedor;
+
+	@Column(name = "correoproveedor")
+	private String correoproveedor;
+
+	@Column(name = "passwordproveedor")
+	private String passwordproveedor;
+
+	@Column(name = "sexo")
+	private String sexo;
+
+	public Integer getIdproveedor() {
+		return idproveedor;
+	}
+
+	public void setIdproveedor(Integer idproveedor) {
+		this.idproveedor = idproveedor;
+	}
+
+	public String getNombreproveedor() {
+		return nombreproveedor;
+	}
+
+	public void setNombreproveedor(String nombreproveedor) {
+		this.nombreproveedor = nombreproveedor;
+	}
+
+	public String getCorreoproveedor() {
+		return correoproveedor;
+	}
+
+	public void setCorreoproveedor(String correoproveedor) {
+		this.correoproveedor = correoproveedor;
+	}
+
+	public String getPasswordproveedor() {
+		return passwordproveedor;
+	}
+
+	public void setPasswordproveedor(String passwordproveedor) {
+		this.passwordproveedor = passwordproveedor;
+	}
+
+	public String getSexo() {
+		return sexo;
+	}
+
+	public void setSexo(String sexo) {
+		this.sexo = sexo;
+	}
 
 }
