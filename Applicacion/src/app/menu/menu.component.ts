138b29e1742aff1235379f5ac4befcc016a617b2
devarenas <78243368+devarenas@users.noreply.github.com>
2021-06-02 09:48:52 -0600

    back and front end updates
    se agrego codigo para usar de observables como prueba en tabla clientes y en el menu component.

--- Applicacion/src/app/menu/menu.component.ts
+++ Applicacion/src/app/menu/menu.component.ts
@@ -1,8 +1,14 @@
 import { Component, OnInit } from '@angular/core';
-import {Observable} from 'rxjs';
+import {BehaviorSubject, Observable} from 'rxjs';
 import {HttpClient} from '@angular/common/http';
 import {HttpHeaders} from '@angular/common/http';
 import {catchError} from 'rxjs/operators';
+
+
+
+
+
+
 
 @Component({
   selector: 'app-menu',
@@ -11,12 +17,60 @@ import {catchError} from 'rxjs/operators';
 })
 export class MenuComponent implements OnInit {
 
-  
 
-  constructor(private http:HttpClient) { }
+  private serviceURL = 'http://localhost:8080/cliente';
+  private mybSubject: BehaviorSubject<any>;
+
+
+
+
+  constructor(private http:HttpClient) { 
+
+    this.mybSubject = new BehaviorSubject(null);
+  }
+
+  
 
   ngOnInit(): void {
 
   }
+
+
+  public getClienteNotification(): Observable<any> {
+
+    return Observable.create((observer: { next: (arg0: any) => void; }) => {
+    const url: any = this.serviceURL+'/notification';
+
+    const eventSource = new EventSource(url);
+
+    eventSource.onmessage = (event) => {
+      console.log('Received event: ', event);
+    };
+
+    eventSource.addEventListener('cliente-result', function (event:any){
+      observer.next(event.data);
+    });
+
+    eventSource.addEventListener('heartbeat-result', function (event){
+      console.log('eventSource.addEventListener: on heartbeat....');
+    });
+
+    return () => eventSource.close();
+  });
+}
+
+
+public doNotificationSubscription(): void {
+  this.mybSubject
+      this.getClienteNotification()
+      .subscribe((result) => {
+
+        console.log('Mensaje recibido:' + JSON.stringify(result));
+        // actualizartabla
+        this.mybSubject.next(result);
+      
+      });
+}
+
 
 }
--- Proyecto/src/main/java/com/application/proyecto/service/ClienteService.java
+++ Proyecto/src/main/java/com/application/proyecto/service/ClienteService.java
@@ -1,9 +1,14 @@
 package com.application.proyecto.service;
 
+import java.time.Duration;
 import java.util.List;
 import java.util.Optional;
+import java.util.UUID;
+
+import javax.annotation.PostConstruct;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.codec.ServerSentEvent;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -16,11 +21,16 @@ import org.springframework.web.bind.annotation.RestController;
 import com.application.proyecto.entity.Cliente;
 import com.application.proyecto.repository.ClienteRepository;
 
+import reactor.core.publisher.EmitterProcessor;
+import reactor.core.publisher.Flux;
+
 
 @RestController
 @RequestMapping("/cliente")
 @CrossOrigin
 public class ClienteService {
+	
+	
 
 	@Autowired
 	ClienteRepository clienteRepository;
@@ -28,13 +38,34 @@ public class ClienteService {
 
 	@GetMapping(path = "/buscar")
 	public List<Cliente> getAllCliente() {
+		
+		
+		
 		return clienteRepository.findAll();
 	}
+	
+	
+	/*@GetMapping(path = "/buscar")
+	public List<Cliente> getAllCliente() {
+		
+		return clienteRepository.findAll();
+	}*/
 
 	@PostMapping(path = "/guardar")
 	public Cliente saveCliente(@RequestBody Cliente cliente) {
+		
+		notificationProcessor.onNext(cliente);
+		
 		return clienteRepository.save(cliente);
+		
+		
 	}
+	
+	/*@PostMapping(path = "/guardar")
+	public Cliente saveCliente(@RequestBody Cliente cliente) {
+		return clienteRepository.save(cliente);
+		
+	}*/
 
 	@DeleteMapping(path = "/eliminar/{idcliente}")
 	public void deleteCliente(@PathVariable("idcliente") Integer idcliente) {
@@ -47,4 +78,55 @@ public class ClienteService {
 			clienteRepository.delete(cliente.get());
 		}
 	}
+	
+	
+	private EmitterProcessor<Cliente> notificationProcessor;
+	
+	@PostConstruct
+	private void createProcessor() {
+		notificationProcessor = EmitterProcessor.<Cliente>create();
+	}
+	
+	
+	
+	private Flux<ServerSentEvent<Cliente>> getClienteSSE() {
+		
+		return notificationProcessor
+				.log()
+				.map(
+						(cliente) -> {
+							System.out.println("Sending cliente:"+ cliente.getNombrecliente());
+							return ServerSentEvent.<Cliente>builder()
+									.id(UUID.randomUUID().toString())
+									.event("cliente-result")
+									.data(cliente)
+									.build();
+						}
+					)
+					.concatWith(Flux.never());
+		
+	}
+	
+	
+	private Flux<ServerSentEvent<Cliente>> getNotificationHeartbeat() {
+        return Flux.interval(Duration.ofSeconds(15))
+                .map(i -> {
+                    System.out.println(String.format("sending heartbeat [%s] ...", i.toString()));
+                    return ServerSentEvent.<Cliente>builder()
+                            .id(String.valueOf(i))
+                            .event("heartbeat-result")
+                            .data(null)
+                            .build();
+                });
+    }
+	
+	@GetMapping(path = "/notification")
+	public Flux<ServerSentEvent<Cliente>> getResultNotification() {
+		return Flux.merge(
+				getClienteSSE(),
+				getNotificationHeartbeat()
+		);
+	}
+	
+
 }
