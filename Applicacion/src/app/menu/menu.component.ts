224bd378fe530f958e2f68cf8a4b5ace14edd00a
Dgo2021 <chinchillaesau@gmail.com>
2021-06-05 12:25:19 -0600

    Delete de espacios

--- Applicacion/src/app/menu/menu.component.ts
+++ Applicacion/src/app/menu/menu.component.ts
@@ -1,9 +1,9 @@
 import { Component, OnInit } from '@angular/core';
-import {BehaviorSubject, Observable} from 'rxjs';
-import {HttpClient} from '@angular/common/http';
-import {HttpHeaders} from '@angular/common/http';
-import {catchError} from 'rxjs/operators';
-import {Subject} from 'rxjs';
+import { BehaviorSubject, Observable } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
+import { HttpHeaders } from '@angular/common/http';
+import { catchError } from 'rxjs/operators';
+import { Subject } from 'rxjs';
 
 
 @Component({
@@ -13,50 +13,39 @@ import {Subject} from 'rxjs';
 })
 export class MenuComponent implements OnInit {
 
-
   private serviceURL = 'http://localhost:8080/cliente';
   private mybSubject: BehaviorSubject<any>;
   private actualizarTexto: any;
   public texto2 = 'nuevo dato ingresado: ';
   private refresh = new Subject<void>();
 
-
   clientes: any = [];
   cliente: any = {};
 
-
-
-
-
-  constructor(private http:HttpClient) { 
+  constructor(private http: HttpClient) {
 
     this.mybSubject = new BehaviorSubject(null);
   }
-
-  
 
   ngOnInit(): void {
     this.buscarClientes();
   }
 
 
-buscarClientes(){
-  this.buscarClientesServicio().subscribe(
-    (response:any)=>this.llenarClientes(response) 
-  )
-}
+  buscarClientes() {
+    this.buscarClientesServicio().subscribe(
+      (response: any) => this.llenarClientes(response)
+    )
+  }
 
-buscarClientesServicio():Observable<any>{
-  return this.http.get<any>("http://localhost:8080/cliente/buscar").pipe(
-    catchError(e=>"error")
-  )
-}
+  buscarClientesServicio(): Observable<any> {
+    return this.http.get<any>("http://localhost:8080/cliente/buscar").pipe(
+      catchError(e => "error")
+    )
+  }
 
-
-llenarClientes(clientes:any){
-  this.clientes = clientes;
-}
-
-
+  llenarClientes(clientes: any) {
+    this.clientes = clientes;
+  }
 
 }
